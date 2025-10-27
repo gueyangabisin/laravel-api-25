7f06169833b594dac6370d3d6cbb3efaabf6a8fe
Muhammad Rizal Indriawan <rizalldisini@gmail.com>
2025-10-27 21:12:56 +0700

    add models

---
+++ app/Models/Books.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Books extends Model
+{
+        protected $guarded = ['id'];
+
+    public function author(){
+        return $this->belongsTo(Authors::class);
+    }
+}
