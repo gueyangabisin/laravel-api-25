3215e733976b7d15928a9873e09f75ca4133f248
Muhammad Rizal Indriawan <rizalldisini@gmail.com>
2025-10-27 20:59:57 +0700

    new migrations

--- database/migrations/2025_10_27_135352_create_products_table.php
+++ database/migrations/2025_10_27_135352_create_products_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('product_category_id')->constrained('category_products')->OnDelete('restrict');
+            $table->string('name');
+            $table->text('description');
             $table->timestamps();
         });
     }
